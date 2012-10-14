80951ab7d8c16cfe9510a329d99b6d018ed0fef1
Sean Cheng <me@remaerd.com>
2012-10-14 10:40:24 +0800

    Initial Commit

---
+++ SCViewController/SCViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCViewController.h
+//  SCViewController
+//
+//  Created by Sean Cheng on 10/14/12.
+//  Copyright (c) 2012 Sean Cheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCViewController : NSObject
+
+@end
---
+++ SCViewController/SCViewController.m
@@ -0,0 +1,13 @@
+//
+//  SCViewController.m
+//  SCViewController
+//
+//  Created by Sean Cheng on 10/14/12.
+//  Copyright (c) 2012 Sean Cheng. All rights reserved.
+//
+
+#import "SCViewController.h"
+
+@implementation SCViewController
+
+@end
